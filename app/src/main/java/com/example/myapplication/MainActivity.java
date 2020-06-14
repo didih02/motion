97a291674b42c614e8b8fa2456eb9f4356fa82c7
didih <diedieh02@gmail.com>
2020-06-14 16:53:30 +0700

    update baru scene 06

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.motion_05_imagefilter);
+        setContentView(R.layout.motion_06_keyframe);
     }
 }
